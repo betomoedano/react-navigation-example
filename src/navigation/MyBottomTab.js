c704faf2a47db3fae9b1630228f0e267c75d27b9
betomoedano <betomoedano@outlook.com>
2022-07-04 12:46:56 -0400

    add bottom tab example

--- src/navigation/MyBottomTab.js
+++ src/navigation/MyBottomTab.js
@@ -3,20 +3,46 @@ import Home from '../screens/Home';
 // import Settings from '../screens/Settings';
 import Profile from '../screens/Profile';
 // import MyStack from './MyStack';
+import { FontAwesome } from '@expo/vector-icons';
+import { Colors } from '../constants/colors';
 
 const MyTab = createBottomTabNavigator();
 
 export default function MyBottomTab() {
   return (
-    <MyTab.Navigator initialRouteName="Home">
-      <MyTab.Screen name="Home" component={Home} />
+    <MyTab.Navigator
+      initialRouteName="Home"
+      screenOptions={{
+        tabBarActiveTintColor: Colors.secondary,
+        headerTitleAlign: 'center',
+      }}
+    >
+      <MyTab.Screen
+        name="Home"
+        component={Home}
+        options={{
+          tabBarIcon: ({ color }) => (
+            <FontAwesome name="home" size={30} color={color} />
+          ),
+        }}
+      />
       {/* we need to change the name */}
       {/* <MyTab.Screen
         name="HomeStack"
         component={MyStack}
         options={{ headerShown: false }}
       /> */}
-      <MyTab.Screen name="Profile" component={Profile} />
+      <MyTab.Screen
+        name="Profile"
+        component={Profile}
+        options={{
+          tabBarBadge: 3,
+          tabBarBadgeStyle: { backgroundColor: 'tomato', color: Colors.ligth },
+          tabBarIcon: ({ color }) => (
+            <FontAwesome name="user" size={28} color={color} />
+          ),
+        }}
+      />
     </MyTab.Navigator>
   );
 }
--- src/navigation/MyStack.js
+++ src/navigation/MyStack.js
@@ -9,7 +9,7 @@ const HomeStack = createStackNavigator();
 
 const myConfig = {
   // title: 'Feed',
-  headerShown: true,
+  headerShown: false,
   headerTitleAlign: 'center',
   presentation: 'modal',
   animationEnabled: true,
@@ -17,9 +17,9 @@ const myConfig = {
   animationTypeForReplace: 'push', //The type of animation to use when this screen replaces another screen
   keyboardHandlingEnabled: true, //the keyboard will NOT automatically dismiss when navigating to a new screen from this screen. Defaults to true.
   //custom header
-  header: ({ navigation, route, options, back }) => (
-    <CustomHeader title={route.name} />
-  ),
+  // header: ({ navigation, route, options, back }) => (
+  //   <CustomHeader title={route.name} />
+  // ),
   // cardStyle: { backgroundColor: 'red' },
 };
 
@@ -52,8 +52,12 @@ export default function MyStack() {
   return (
     <HomeStack.Navigator screenOptions={myConfig}>
       <HomeStack.Screen name="Root" component={MyBottomTab} />
-      <HomeStack.Group>
-        <HomeStack.Screen name="Settings" component={Settings} />
+      <HomeStack.Group screenOptions={{ headerShown: true }}>
+        <HomeStack.Screen
+          name="Settings"
+          component={Settings}
+          options={{ headerBackTitle: 'Home' }}
+        />
       </HomeStack.Group>
     </HomeStack.Navigator>
   );
