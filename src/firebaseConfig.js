6abef182b9edb0afe0e7375dbf922cdb6789a43e
betomoedano <betomoedano@outlook.com>
2022-08-04 21:06:35 -0400

    upgrade to expo 46 and connect to firebase

---
+++ src/firebaseConfig.js
@@ -0,0 +1,19 @@
+import { initializeApp } from 'firebase/app';
+//import {...} from "firebase/auth";
+//import {...} from "firebase/database";
+//import {...} from "firebase/firestore";
+//import {...} from "firebase/functions";
+//import {...} from "firebase/storage";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: '',
+  authDomain: '',
+  projectId: '',
+  storageBucket: '',
+  messagingSenderId: '',
+  appId: '',
+};
+
+// Initialize Firebase
+initializeApp(firebaseConfig);
