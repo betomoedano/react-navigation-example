44fdf711778ebab6854c579e7a8c272abc1632c9
betomoedano <betomoedano@outlook.com>
2022-08-06 12:34:08 -0400

    fix

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -6,23 +6,13 @@ import { getAuth } from 'firebase/auth';
 //import {...} from "firebase/storage";
 
 // Your web app's Firebase configuration
-// const firebaseConfig = {
-//   apiKey: '',
-//   authDomain: '',
-//   projectId: '',
-//   storageBucket: '',
-//   messagingSenderId: '',
-//   appId: '',
-// };
-
 const firebaseConfig = {
-  apiKey: 'AIzaSyBXcLfR0FXmPhHHXu1iRdiRteJl1EJBKKo',
-  authDomain: 'test-d8eca.firebaseapp.com',
-  databaseURL: 'https://test-d8eca-default-rtdb.firebaseio.com',
-  projectId: 'test-d8eca',
-  storageBucket: 'test-d8eca.appspot.com',
-  messagingSenderId: '973948352506',
-  appId: '1:973948352506:web:db70d2d883546070141643',
+  apiKey: '',
+  authDomain: '',
+  projectId: '',
+  storageBucket: '',
+  messagingSenderId: '',
+  appId: '',
 };
 
 // Initialize Firebase
